735bdbf3ac0019ef6d847c6fc4ef36666ff0f7eb
Julien Sellier <juju.sel.mail@gmail.com>
2023-07-29 16:29:08 +0200

    add domain scanning

--- pkg/hunt/tcp.go
+++ pkg/hunt/tcp.go
@@ -11,10 +11,11 @@ import (
 	"time"
 )
 
-type TCPPortScanResult struct {
+type TCPAddrInfo struct {
+	Addr              *net.TCPAddr
 	At                time.Time
-	ConnError         string
 	State             TCPPortState
+	ConnError         string
 	Banner            []byte
 	PotentialServices []Service
 	ConfirmedService  Service
@@ -87,22 +88,23 @@ var CommonTCPPorts = [65536][]Service{
 	9100: {SrvNodeExporter},
 }
 
-func ScanTCPPort(target *net.TCPAddr, connTimeout time.Duration, httpHost string) (*TCPPortScanResult, error) {
+// ScanTCP always returns a result.
+func ScanTCP(target *net.TCPAddr, connTimeout time.Duration, detectors ...Detector) *TCPAddrInfo {
 	if connTimeout == 0 {
 		connTimeout = time.Second
 	}
-	result := &TCPPortScanResult{At: time.Now()}
+	result := &TCPAddrInfo{Addr: target, At: time.Now()}
 	raddr := target.String()
 	conn, err := net.DialTimeout("tcp", raddr, connTimeout)
 	if err != nil {
 		if errors.Is(err, syscall.ECONNREFUSED) {
 			result.State = TCPPortStateClosed
-			return result, nil // Connection was explicitly refused, stop here.
+			return result // Connection was explicitly refused, stop here.
 		}
 		// Other errors could be: unreachable host, timeout, etc.
 		result.State = TCPPortStateUnknown
 		result.ConnError = err.Error()
-		return result, nil
+		return result
 	}
 	defer conn.Close()
 	result.State = TCPPortStateOpen
@@ -122,40 +124,81 @@ func ScanTCPPort(target *net.TCPAddr, connTimeout time.Duration, httpHost string
 		}
 	}
 
-	// Check if remote port speaks HTTP
-	if SpeaksHTTP(conn, httpHost) {
-		result.ConfirmedService = SrvHTTP
+	// Try to detect service
+	result.ConfirmedService = SrvUnknown
+	for _, detect := range detectors {
+		result.ConfirmedService = detect(conn)
+		if result.ConfirmedService != SrvUnknown {
+			return result // stop here if we detected the specific service running on port.
+		}
 	}
-
-	return result, nil
+	return result
 }
 
+type Detector func(conn net.Conn) Service
+
 // Sends a HTTP request to a remote server and reports whether the server successfully replied in HTTP.
-func SpeaksHTTP(conn net.Conn, httpHost string) bool {
-	if httpHost == "" {
-		panic(errors.New("HTTP host is mandatory"))
-	}
-	req, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil {
-		panic(fmt.Errorf("unexpected malformed request: %w", err))
-	}
-	req.Host = httpHost
-	rawReq, err := httputil.DumpRequest(req, false)
-	if err != nil {
-		panic(fmt.Errorf("unexpected request dump fail: %w", err))
-	}
+func DetectHTTP(httpHost string, ua string) Detector {
+	return func(conn net.Conn) Service {
+		if httpHost == "" {
+			panic(errors.New("HTTP host is mandatory"))
+		}
+		if ua == "" {
+			ua = RandomUserAgent()
+		}
+		req, err := http.NewRequest(http.MethodGet, "/", nil)
+		if err != nil {
+			panic(fmt.Errorf("unexpected malformed request: %w", err))
+		}
+		req.Host = httpHost
+		req.Header.Set("User-Agent", ua)
+		rawReq, err := httputil.DumpRequest(req, false)
+		if err != nil {
+			panic(fmt.Errorf("unexpected request dump fail: %w", err))
+		}
 
-	_ = conn.SetWriteDeadline(time.Now().Add(5 * time.Second))
-	_, err = conn.Write(rawReq)
-	if err != nil {
-		return false
-	}
+		_ = conn.SetWriteDeadline(time.Now().Add(5 * time.Second))
+		_, err = conn.Write(rawReq)
+		if err != nil {
+			return SrvUnknown
+		}
 
-	_ = conn.SetReadDeadline(time.Now().Add(time.Second))
-	res, err := http.ReadResponse(bufio.NewReader(conn), req)
-	if err != nil {
-		return false
+		_ = conn.SetReadDeadline(time.Now().Add(time.Second))
+		res, err := http.ReadResponse(bufio.NewReader(conn), req)
+		if err != nil {
+			return SrvUnknown
+		}
+		defer res.Body.Close()
+		return SrvHTTP
 	}
-	defer res.Body.Close()
-	return true
+}
+
+var AllPorts = PortsBetween(1, 65535)
+
+// Note: range includes "from" and "to".
+func PortsBetween(from, to uint16) []uint16 {
+	out := make([]uint16, 0, to-from)
+	for i := from; i <= to; i++ {
+		out = append(out, uint16(i))
+	}
+	return out
+}
+
+func CommonPorts() []uint16 {
+	out := []uint16{}
+	for port, srvs := range CommonTCPPorts {
+		if srvs == nil {
+			continue
+		}
+		out = append(out, uint16(port))
+	}
+	return out
+}
+
+func TCPAddresses(ipAddr net.IP, ports ...uint16) []*net.TCPAddr {
+	out := make([]*net.TCPAddr, len(ports))
+	for i, port := range ports {
+		out[i] = &net.TCPAddr{IP: ipAddr, Port: int(port)}
+	}
+	return out
 }
